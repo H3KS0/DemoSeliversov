6cc27e4f1dd29a5534898e1846ef7947b128aa08
H3KS0 <128075535+H3KS0@users.noreply.github.com>
2023-12-08 22:03:53 +0500

    Add files via upload

---
+++ ДЕМОЭКЗАМЕН3/ДЕМОЭКЗАМЕН3/BasketForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ДЕМОЭКЗАМЕН3
+{
+    public partial class BasketForm : Form
+    {
+        static string connString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Demo_Seliverstov;Data Source=DESKTOP-ALHNBP8\SQLEXPRESS";
+        SqlConnection SqlConnection = new SqlConnection(connString);
+        public int basket_id;
+        public int basket_role;
+        public BasketForm(int current_role, int current_id)
+        {
+            basket_role = current_role;
+            basket_id = current_id;
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Application.OpenForms[1].Show();
+        }
+    }
+}
---
+++ ДЕМОЭКЗАМЕН3/ДЕМОЭКЗАМЕН3/OrdersForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ДЕМОЭКЗАМЕН3
+{
+    public partial class OrdersForm : Form
+    {
+        static string connString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=Demo_Seliverstov;Data Source=DESKTOP-ALHNBP8\SQLEXPRESS";
+        SqlConnection SqlConnection = new SqlConnection(connString);
+        public int order_id;
+        public int order_role;
+        public OrdersForm(int current_role, int current_id)
+        {
+            order_role = current_role;
+            order_id = current_id;
+            InitializeComponent();
+        }
+        private void OrdersForm_load(object sender, EventArgs e)
+        {
+            //select с вложенным запросом
+            SqlCommand sqlCommand = new SqlCommand("SELECT o.OrderID, o.DeliveryData, o.CodeToGet, s.ProductArticleNumber, (SELECT PointOfIssue as pointofissue FROM dbo.PointOfIssue WHERE IssueID = PointIssue) FROM dbo.[Order] as o INNER JOIN  dbo.[OrderProduct] as s on (s.OrderID = o.OrderID)", SqlConnection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlCommand);
+            DataSet dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+            ProductGridView.DataSource = dataSet.Tables[0];
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Application.OpenForms[1].Show();
+        }
+    }
+}
